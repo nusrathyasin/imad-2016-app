961252cca525af2572dcdfe2cc65f90437b65416
nusrathyasin <nusrathyasin000@gmail.com>
2016-09-20 19:29:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'ui', 'index1.html'));
 });
 
-app.get('/ui/file1.css', function (req, res) {
+app.get('/file1.css', function (req, res) {
   res.sendFile(path.join(__dirname,'ui', 'file1.css'));
 });
-app.get('/ui/article1.html', function (req, res) {
+app.get('/article1.html', function (req, res) {
   res.sendFile(path.join(__dirname,'ui', 'article1.html'));
 });
 
