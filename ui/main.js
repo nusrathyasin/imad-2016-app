1560c3cce8250747afaae903ef8a2d7533dea606
nusrathyasin <nusrathyasin000@gmail.com>
2016-09-30 20:41:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,10 @@ button.onclick = function() {
             var counter = request.responseText;
             var span = document.getElementById('count');
             span.innerHTML = counter.toString();
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
             
+    
         }
     }
 };
