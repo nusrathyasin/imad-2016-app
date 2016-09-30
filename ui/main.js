3aa7152dd44fcd432ac52e7d5dda4dccec5d7a4f
nusrathyasin <nusrathyasin000@gmail.com>
2016-09-30 21:34:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var button = document.getElementById('counter');
 //var counter=0;
 button.onclick = function() {
    // counter = counter+1;
-   
+   console.log("hoo");
     //Create a request object
     var request = new XMLHttpRequest();
     
