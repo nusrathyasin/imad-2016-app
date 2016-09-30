f44025271c1787a2575b2413112019f022cf93e4
nusrathyasin <nusrathyasin000@gmail.com>
2016-09-30 11:45:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'ui', 'index1.html'));
 });
 
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
 app.get('/ui/file1.css', function (req, res) {
   res.sendFile(path.join(__dirname,'ui', 'file1.css'));
 });
