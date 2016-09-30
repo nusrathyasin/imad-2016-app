5ac006e545dd7c97e960f0998e19695f29c3766c
nusrathyasin <nusrathyasin000@gmail.com>
2016-09-30 20:12:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,23 @@
 //counter code
 var button = document.getElementById('counter');
+var counter=0;
 button.onClick = function() {
-    
+    counter = counter+1;
     //Create a request object
-    var request = new XMLHttprequest();
+   // var request = new XMLHttprequest();
     
     //Capture the response and store it in a variable
-    request.onReadyStateChange = function() {
-        if(request.readyState === XMLHttpRequest.DONE){
-        if(request.status ===200){
-            var counter = request.responseText;
+   // request.onReadyStateChange = function() {
+     //   if(request.readyState === XMLHttpRequest.DONE){
+      //  if(request.status ===200){
+      //      var counter = request.responseText;
             var span = document.getElementById('count');
             span.innerHTML = counter.toString();
             
-        }
-    }
+       // }
+   // }
 };
 //make the request
-request.open('GET','http://nusrathyasin.imad.hasura-app.io/counter',true);
-request.send(null);
-};
+//request.open('GET','http://nusrathyasin.imad.hasura-app.io/counter',true);
+//request.send(null);
+//};
