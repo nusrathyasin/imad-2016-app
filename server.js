3f219eb3b1904c53bd398109eba1981a343ab91a
nusrathyasin <nusrathyasin000@gmail.com>
2016-09-30 20:19:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'ui', 'index1.html'));
 });
 
-var counter = 0;
-app.get('/counter',function(req,res){
-    counter = counter + 1;
-    res.send(counter.toString());
-});
+
 app.get('/ui/file1.css', function (req, res) {
   res.sendFile(path.join(__dirname,'ui', 'file1.css'));
 });
@@ -29,14 +25,20 @@ app.get('/ui/article2.html', function (req, res) {
 app.get('/ui/article3.html', function (req, res) {
   res.sendFile(path.join(__dirname,'ui', 'article3.html'));
 });
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui', 'main.js'));
-});
 
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname,'ui', 'main.js'));
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
