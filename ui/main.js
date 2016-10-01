8bfa5480687091aeb01df93625e27de5076bbbc0
nusrathyasin <nusrathyasin000@gmail.com>
2016-10-01 12:07:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,30 @@ request.open('GET','http://nusrathyasin.imad.hasura-app.io/counter',true);
 request.send(null);
 };
 
+//comment box
+var submit =document.getElementById('submit_btn');
+submit.onclick = function() {
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture response nd store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status ===200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++){
+                    list += '<li>' + names[i] + '<li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            
+        
+                }
+            }
+        };
+var nameInput =document.getElementById('name'); 
+var name = nameInput.value;
+request.open('GET','http://nusrathyasin.imad.hasura-app.io/submit-name?name=' + name,true);
+request.send(null);
+};
