6c1eb41f6da51890736678a48c2e9edf8d2c855f
nusrathyasin <nusrathyasin000@gmail.com>
2016-10-01 18:55:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,10 +41,7 @@ submit.onclick = function() {
                     list += '<li style="border-bottom:solid">' + names[i] + '</li>';
                     
                 }
-                document.write ('<p>Current time is: <span id="date-time">', new Date().toLocaleString(), '<\/span>.<\/p>')
-if (document.getElementById) onload = function () {
-	setInterval ("document.getElementById ('date-time').firstChild.data = new Date().toLocaleString()", 50)
-}
+               
                 var ul = document.getElementById('namelist');
                 ul.innerHTML = list;
             
