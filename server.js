4178290282df11c532b5df68ba979c90e20a9328
Nusrath <nusrathyasin000@gmail.com>
2016-11-20 18:34:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ app.get('/', function (req, res) {
 });
 
 function hash (input,salt) {
-    var hashed =  crypto.pbkdf2Sync('input', 'salt', 100000, 512, 'sha512');
+    var hashed =  crypto.pbkdf2Sync('input', 'salt', 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
 
 app.get('/hash/:input', function(req,res) {
-    var hashedString = hash(req.params.input,'this-is-some-random-string');
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
 });
 
