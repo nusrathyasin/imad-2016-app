a1c5c5d69b76004039ec1f8b9299fbf592193d01
Nusrath <nusrathyasin000@gmail.com>
2016-11-20 20:40:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,6 +27,37 @@ request.open('GET','http://nusrathyasin.imad.hasura-app.io/counter',true);
 request.send(null);
 };
 
+//login
+var submit =document.getElementById('submt_btn');
+submit.onclick = function() {
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture response nd store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                alert('Logged in successfully');
+            } else if (request.status === 403) {
+                alert('Username/Password is incorrect');
+            } else if (request.status === 500){
+                alert('Something goes wrong on the server');
+                }
+            }
+        };
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST','http://nusrathyasin.imad.hasura-app.io/login' ,true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({username: username, password: password}));
+
+};
+
+
+
+
+
 //comment box
 var submit =document.getElementById('submit_btn');
 submit.onclick = function() {
