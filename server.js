7ea4ea2e8a969d65e7a29b8940c024b47922668d
Nusrath <nusrathyasin000@gmail.com>
2016-11-21 10:26:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'nusrathyasin',
@@ -16,6 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'spmeRandomSecretValue',
+    cookie:{ maxAgge: 1000 * 60 * 60 * 24 * 30}
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'ui', 'index.html'));
