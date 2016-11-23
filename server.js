9c9a0bd51ef2f5f598187869277c9118ca2f05c1
Nusrath <nusrathyasin000@gmail.com>
2016-11-23 16:48:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(session({
 }));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui', 'index1.html'));
+  res.sendFile(path.join(__dirname,'ui', 'index.html'));
 });
 
 function hash (input,salt) {
