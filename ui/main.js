ed769ad0af41a554d5c0ccd413e34b093e848980
Nusrath <nusrathyasin000@gmail.com>
2016-11-23 22:57:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,6 @@
 
 
-//counter code
-var button = document.getElementById('counter');
-//var counter=0;
-button.onclick = function() {
-   // counter = counter+1;
-    //Create a request object
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE){
-        if(request.status ===200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-            
-            
 
-        }
-    }
-    
-};
-//make the request
-request.open('GET','http://nusrathyasin.imad.hasura-app.io/counter',true);
-request.send(null);
-};
 
 //login
 function loadLoginForm () {
@@ -137,7 +112,32 @@ function loadLogin () {
 loadLogin();
 
 
+//counter code
+var button = document.getElementById('counter');
+//var counter=0;
+button.onclick = function() {
+   // counter = counter+1;
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+        if(request.status ===200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+            
+            
 
+        }
+    }
+    
+};
+//make the request
+request.open('GET','http://nusrathyasin.imad.hasura-app.io/counter',true);
+request.send(null);
+};
 
 //comment box
 var submit =document.getElementById('submit_btn');
