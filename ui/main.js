53f93323605d838be75ec3b8cc871a2fa60e6a0a
Nusrath <nusrathyasin000@gmail.com>
2016-11-24 17:35:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -64,8 +64,9 @@ function loadLoginForm () {
               // Take some action
               if (request.status === 200) {
                   alert('User created successfully');
+                                    window.open('http://nusrathyasin.imad.hasura-app.io/ui/homepage');
                        register.value = 'Registered!';
-                  window.open('http://nusrathyasin.imad.hasura-app.io/ui/homepage');
+
              
               } else {
                   alert('Could not register the user');
