e9e79737661a57440ba243158f3fa25c508d534d
Nusrath <nusrathyasin000@gmail.com>
2016-11-24 15:18:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -103,7 +103,7 @@ function loadLogin () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
                  
-                loadLoggedInUser(this.responseText);
+                //loadLoggedInUser(this.responseText);
                 request.open('GET', 'http://nusrathyasin.imad.hasura-app.io/ui/homepage.html', true);
     request.send(null);
                 
