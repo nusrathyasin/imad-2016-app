eb33e673025cdb680910b9338fada6461da25bea
Nusrath <nusrathyasin000@gmail.com>
2016-11-25 13:48:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -183,5 +183,36 @@ request.open('GET','http://nusrathyasin.imad.hasura-app.io/counter1',true);
 request.send(null);
 };
 //
+var submit =document.getElementById('submit_btn');
+submit.onclick = function() {
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture response nd store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status ===200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                
+                
+                for(var i=0;i<names.length;i++){
+                    list += '<li  class="commentbox">' + names[i] + '</li>';
+                    
+                }
+                
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            
+        
+                }
+            }
+        };
+var nameInput =document.getElementById('name');
+var name = nameInput.value;
+request.open('GET','http://nusrathyasin.imad.hasura-app.io/submit-name?name=' + name,true);
+request.send(null);
+
+};
 
 
