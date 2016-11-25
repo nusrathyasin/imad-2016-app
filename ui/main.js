1d1c79366a21758803be51ffbc1c2cc103c3f0ef
Nusrath <nusrathyasin000@gmail.com>
2016-11-25 12:34:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -184,83 +184,14 @@ request.send(null);
 };
 //
 
-function loadCommentForm () {
-    var commentFormHtml = `
-        <h5>Submit a comment</h5>
-        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-        <br/>
-        <input type="submit" id="submit" value="Submit" />
-        <br/>
-        `;
-    document.getElementById('comment_form').innerHTML = commentFormHtml;
-    
-    // Submit username/password to login
-    var submit = document.getElementById('submit');
-    submit.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-                // Take some action
-                if (request.status === 200) {
-                    // clear the form & reload all the comments
-                    document.getElementById('comment_text').value = '';
-                    loadComments();    
-                } else {
-                    alert('Error! Could not submit comment');
-                }
-                submit.value = 'Submit';
-          }
-        };
-        
-        // Make the request
-        var comment = document.getElementById('name').value;
-        request.open('POST', '/submit-comment/' , true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({comment: comment}));  
-        submit.value = 'Submitting...';
-        
-    };
-}
-function loadComments () {
-        // Check if the user is already logged in
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            var comments = document.getElementById('comments');
-            if (request.status === 200) {
-                var content = '';
-                var commentsData = JSON.parse(this.responseText);
-                for (var i=0; i< commentsData.length; i++) {
-                    var time = new Date(commentsData[i].timestamp);
-                    content += `<div class="comment">
-                        <p style="color:#550C73">${escapeHTML(commentsData[i].comment)}</p>
-                        <div class="commenter">
-                            <div style='color:#04d99d;font-weight:bold;font-variant: small-caps'>${escapeHTML(commentsData[i].username)}</div> - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()}
-                        </div>
-                    </div>`;
-                }
-                comments.innerHTML = content;
-            } else {
-                comments.innerHTML('Oops! Could not load comments!');
-            }
-        }
-    };
-    
-    request.open('GET', '/get-comments/', true);
-    request.send(null);
-}
 
-loadComments();
 //
 //comment box
 var submit =document.getElementById('submit_btn');
 submit.onclick = function() {
-    create a request object
-   var request = new XMLHttpRequest();
-    capture response nd store it in a variable
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture response nd store it in a variable
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status ===200){
@@ -271,8 +202,6 @@ submit.onclick = function() {
                 
                 for(var i=0;i<names.length;i++){
                     list += '<li  class="commentbox">' + names[i] + '</li>';
-                 <div style='color:#04d99d;font-weight:bold;font-variant: small-caps'>${escapeHTML(commentsData[i].username)}</div> - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()}
-                        </div>   
                     
                 }
                 
@@ -283,9 +212,9 @@ submit.onclick = function() {
                 }
             }
         };
-//var nameInput =document.getElementById('name');
-//var name = nameInput.value;
-//request.open('GET','http://nusrathyasin.imad.hasura-app.io/submit-name?name=' + name,true);
-//request.send(null);
+var nameInput =document.getElementById('name');
+var name = nameInput.value;
+request.open('GET','http://nusrathyasin.imad.hasura-app.io/submit-name?name=' + name,true);
+request.send(null);
 
 };
