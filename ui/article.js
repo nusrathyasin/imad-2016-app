1c36615c150caeaf966bcdbd0e10fbdc59f1e4f4
Nusrath <nusrathyasin000@gmail.com>
2016-11-26 07:03:15 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -90,7 +90,7 @@ function loadComments () {
         }
     };
     
-    request.open('GET', '/get-comments/' + currentArticleTitle, true);
+    request.open('GET', '/get-comments/', true);
     request.send(null);
 }
 
