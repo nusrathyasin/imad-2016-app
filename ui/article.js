e60e4f6cd46dec444dd399242d1f35b15da92e5a
Nusrath <nusrathyasin000@gmail.com>
2016-11-26 07:07:54 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -42,7 +42,28 @@ function loadCommentForm () {
     };
 }
 
+function loadLogin () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadCommentForm(this.responseText);
+            }
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
 
+function escapeHTML (text)
+{
+    var $text = document.createTextNode(text);
+    var $div = document.createElement('div');
+    $div.appendChild($text);
+    return $div.innerHTML;
+}
 
 function loadComments () {
         // Check if the user is already logged in
@@ -75,5 +96,5 @@ function loadComments () {
 
 
 // The first thing to do is to check if the user is logged in!
-
+loadLogin();
 loadComments();
